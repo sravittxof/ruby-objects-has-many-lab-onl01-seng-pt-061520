c1c91b73095967a32d1808f06e89b2fbb84f5db4
Travis Fox <travis.fox90@gmail.com>
2020-07-19 17:27:50 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,20 @@
 class Post
   
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    
+  end
+  
 end
